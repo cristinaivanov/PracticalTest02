dcb372a3a2a7733e4bace6d3b62ca00cca6626d9
Cristina <ivanov.cristina99@gmail.com>
2021-06-03 10:20:38 +0300

    ex 1

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/MainActivity.java
@@ -3,12 +3,24 @@ package ro.pub.cs.systems.eim.practicaltest02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText hourEditText;
+    private EditText minuteEditText;
+    private Button setButton, resetButton, pollButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_practical_test02_main);
+
+        hourEditText = (EditText)findViewById(R.id.editTextTextPersonName);
+        minuteEditText = (EditText)findViewById(R.id.editTextTextPersonName2);
+
+        setButton = (Button)findViewById(R.id.button);
+        resetButton = (Button)findViewById(R.id.button2);
+        pollButton = (Button)findViewById(R.id.button3);
     }
 }
